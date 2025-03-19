7da800483cc1cbb1a8429ce3e0d3234ba9b8078e
anant-jain01 <jain.anant609@gmail.com>
2025-03-19 09:45:15 +0530

    new file:   $PROFILE.txt 	modified:   index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
-const App = () => {return(
-  <div>
-    <h1>Padre Gino's Pizza – Order Now</h1>
-    <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
-    <Pizza
-      name="The Hawaiian Pizza"
-      description="Sliced Ham, Pineapple, Mozzarella Cheese"
-    />
-    <Pizza
-      name="The Big Meat Pizza"
-      description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
-    />
-  </div>)
+const App = () => {
+  return (
+    <div>
+      <h1>Padre Gino's Pizza – Order Now</h1>
+      <Pizza
+        image={"/public/pizzas/pepperoni.webp"}
+        name="Pepperoni"
+        description="Mozzarella Cheese, Pepperoni"
+      />
+      <Pizza
+        image={"/public/pizzas/hawaiian.webp"}
+        name="The Hawaiian Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+      />
+      <Pizza
+        image={"/public/pizzas/big_meat.webp"}
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+      />
+    </div>
+  );
 };
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
-root.render(<App/>);
+root.render(<App />);
