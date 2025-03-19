4d7ecd2f00c038ff4c5544c242efe1cde3163ec1
anant-jain01 <jain.anant609@gmail.com>
2025-03-19 09:52:27 +0530

    modified:   src/Pizza.jsx

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,10 +1,11 @@
 const Pizza = (props) => {
   return (
-    <div className="Pizza">
+    <div className="pizza">
       <h1>{props.name}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
     </div>
   );
 };
+
 export default Pizza;
